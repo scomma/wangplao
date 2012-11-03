c99554c7338a92df20098be683406cf21c991749
Chakrit Wichian <chakrit@oozou.com>
2012-11-04 03:44:45 +0700

    Remove Calendars#index mock method.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,11 +1,4 @@
 class CalendarsController < ApplicationController
-  def index
-    mock_range = (Date.today - 3.days)...(Date.today + 3.days)
-    @availabilities = mock_range.map do |date|
-      Availability.new :on => date
-    end
-  end
-
   def show
     @calendar = Calendar.find_by_token!(params[:id])
   end

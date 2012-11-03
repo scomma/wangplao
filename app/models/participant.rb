451cb38340f5e36a17c55ab8794b3c625eb124a8
Prathan Thananart <pop@oozou.com>
2012-11-04 01:40:20 +0700

    Model pleasantries

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,5 +2,10 @@ class Participant < ActiveRecord::Base
   belongs_to :calendar, inverse_of: :participants
   has_many :availabilities, inverse_of: :participant
 
-  attr_accessible :name
+  attr_accessible :name, :availabilities_attributes
+  accepts_nested_attributes_for :availabilities, allow_destroy: true
+
+  def to_s
+    name
+  end
 end

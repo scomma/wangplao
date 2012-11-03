2642b847c9fb9e24c5cc5e15371177bb6af988ce
Prathan Thananart <pop@oozou.com>
2012-11-04 01:24:44 +0700

    Route proposal

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,5 +1,23 @@
 class Calendar < ActiveRecord::Base
   has_many :participants, inverse_of: :calendar
+  has_many :availabilities, through: :participants
 
-  attr_accessible :description, :range_end, :range_start, :title, :token
+  validates_presence_of :range_start, :range_end
+
+  before_create :generate_token
+
+  attr_accessible :description, :range_end, :range_start, :title
+
+  def to_s
+    title.presence || "Calendar"
+  end
+
+  def to_param
+    token
+  end
+
+private
+  def generate_token
+    self.token = SecureRandom.hex
+  end
 end
